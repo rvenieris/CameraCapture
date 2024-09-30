d3654f0dc4bec78aacc27f1753994c5c514c1f3d
Ricardo Venieris <rvenieris@gmail.com>
2024-09-30 10:17:47 -0300

    Initial Commit

---
+++ CameraCaptureTests/CameraCaptureTests.swift
@@ -0,0 +1,17 @@
+//
+//  CameraCaptureTests.swift
+//  CameraCaptureTests
+//
+//  Created by Ricardo Almeida Venieris on 30/09/24.
+//
+
+import Testing
+@testable import CameraCapture
+
+struct CameraCaptureTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
